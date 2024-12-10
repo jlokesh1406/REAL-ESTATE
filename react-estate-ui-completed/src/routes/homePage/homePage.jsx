35055a0bec36fa30994a1a13fa1c93a82a58e4c1
LOKESH J <153013792+jlokesh1406@users.noreply.github.com>
2024-12-11 00:51:32 +0530

    Add files via upload

---
+++ react-estate-ui-completed/src/routes/homePage/homePage.jsx
@@ -0,0 +1,23 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            explicabo suscipit cum eius, iure est nulla animi consequatur
+            facilis id pariatur fugit quos laudantium temporibus dolor ea
+            repellat provident impedit!
+          </p>
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
